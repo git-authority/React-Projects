7eb30055c4999669e2e12d9c79eb4c4445fef977
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-04 22:13:43 +0530

    Learned an important thing about component

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   const [counter, setCounter] = useState(15);
 
   const addValue = () => {
-    setCounter(counter+1);
+    setCounter(prevCounter => prevCounter+1);
   }
 
   const removeValue = () => {
-    setCounter(counter-1);
+    setCounter(prevCounter => prevCounter-1);
   }
 
   return (
