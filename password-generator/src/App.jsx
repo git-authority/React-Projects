efec95b0684244dbfb0604b2ab5fdfaec7297bfb
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-06 23:36:08 +0530

    Implemented a Simple Password Generator

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("")
+
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -27,57 +29,95 @@ function App() {
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+
+  const copyPasswordToClipboard = useCallback(()=> {
+    window.navigator.clipboard.writeText(password);
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed])
+
   return (
     <>
 
-      {/* Viewport */}
+      {/* ------------------------------------- Viewport --------------------------- */}
 
       <div className='min-h-screen flex items-center justify-center'>
 
 
-        {/* div for outer grey box which contains everything */}
-
-        <div className="flex items-center justify-center w-full h-40 max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-slate-600 bg-gray-800">
 
 
-          {/* div for enabling the input field and checkboxes to be in different rows */}
+
+        {/* ----------------- div for outer grey box which contains everything ------------------- */}
+
+        <div className="flex items-center justify-center w-full h-45 max-w-lg mx-auto shadow-md rounded-lg px-4 my-8 text-slate-600 bg-gray-800">
+
+
+
+
+          {/* ---------------- div for enabling the input field and checkboxes to be in different rows ------------- */}
 
           <div className='flex items-center flex-col w-full'>
 
 
-            {/* Row 1  ->  Text - Password Generator */}
+
+
+
+            {/* ------------------------ Row 1  ->  Text - Password Generator -------------------- */}
 
             <h1 className="text-white text-center mb-8">Password Generator</h1>
 
+            {/* ---------------------------- Row 1 ends ---------------------------- */}
 
 
 
-            {/* Row 2  ->  Textbox and Copy Button */}
+
+
+
+            {/* ---------------- Row 2  ->  Textbox and Copy Button ----------------- */}
 
             <div className="flex shadow rounded-lg overflow-hidden w-full">
 
-              {/* Textbox for Displaying Generated Password */}
-              <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white text-center' placeholder='Password' readOnly />
 
-              {/* Copy Button */}
-              <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer'>Copy</button>
+
+              {/* ----------------------- Textbox for Displaying Generated Password ---------------- */}
+              <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white text-center text-orange-600' placeholder='Password' readOnly ref={passwordRef}/>
+
+
+
+              {/* --------------------------------- Copy Button ------------------------------------ */}
+              <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer'>Copy</button>
+
+
 
             </div>
 
 
+            {/* ------------------------- Rows 2 ends ------------------------- */}
 
 
-            {/* Row 3  -> Outer div for Slider, Number Checkbox, Input Checkbox */}
+
+
+
+
+
+
+            {/* -------------------- Row 3  -> Outer div for Slider, Number Checkbox, Input Checkbox ------------------------ */}
 
             <div className="w-full text-left mt-4">
 
 
-              {/* Row 3  -> Inner div for Slider, Number Checkbox, Input Checkbox */}
+
+
+              {/* --------------- Row 3  -> Inner div for Slider, Number Checkbox, Input Checkbox ---------------- */}
 
               <div className="flex items-center gap-3">
 
 
-                {/* Slider for Length of Password to be Generated */}
+
+
+                {/* ------------------------- Slider for Length of Password to be Generated ------------------------- */}
 
                 <input
                   type="range"
@@ -85,18 +125,24 @@ function App() {
                   max={100}
                   value={length}
                   onChange={(e) => setLength(e.target.value)}
-                  className="w-[100px] h-1 cursor-pointer"
+                  className="w-[100px] h-1 cursor-pointer accent-orange-400"
                 />
 
 
-                {/* Label for Length */}
+
+
+
+
+                {/* ------------------------------- Label for Length of Slider ---------------------------------- */}
 
                 <label className="text-orange-400">Length: {length}</label>
 
 
 
 
-                {/* Checkbox for Allowing Numbers in Password (with its separate div) */}
+
+
+                {/* ------------------- Checkbox for Allowing Numbers in Password (with its separate div) ------------------- */}
 
                 <div className="flex items-center gap-x-1">
 
@@ -108,11 +154,14 @@ function App() {
 
                 </div>
 
+                {/* ------------------- Checkbox for Allowing Numbers in Password (with its separate div) ends --------------- */}
 
 
 
 
-                {/* Checkbox for Allowing Characters in Password (with its separate div) */}
+
+
+                {/* --------------- Checkbox for Allowing Special Characters in Password (with its separate div) --------------------*/}
 
                 <div className="flex items-center gap-x-1">
 
@@ -120,29 +169,43 @@ function App() {
                     setCharAllowed((prev) => !prev)
                   }} />
 
-                  <label className="text-orange-400" htmlFor="characterInput">Characters</label>
+                  <label className="text-orange-400" htmlFor="characterInput">Special Characters</label>
 
                 </div>
 
-
-
+                {/* ---------------- Checkbox for Allowing Special Characters in Password (with its separate div) ends -----------------*/}
 
 
 
               </div>
+
+              {/* ------------------------------------ Row 3 inner div ends ------------------------------------ */}
 
 
 
 
             </div>
 
+            {/* -------------------------------------- Row 3 outer div ends ------------------------------------------------- */}
+
+
 
 
           </div>
 
 
+          {/* ---------------- div for enabling the input field and checkboxes to be in different rows ends ------------- */}
+
+
         </div>
+
+        {/* -------------------------------------- Outer Grey Box Ends -------------------------------------- */}
+
+
       </div>
+
+      {/* ---------------------------------------------- Viewport ends -------------------------------------------------- */}
+
     </>
   )
 }
