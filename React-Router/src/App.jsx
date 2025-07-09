3ac235eabf14a7f80384da637604a9dabd9a5c84
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-09 12:41:42 +0530

    Adding Basic Pages using Router

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
+import Contact from './components/Contact'
+import { Link, NavLink } from 'react-router-dom'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
