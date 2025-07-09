3aea83c92da1fc849aacf30c272368f418d87f5b
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-10 00:25:33 +0530

    Made some changes in Header and Improved Code Readability

--- React-Router/src/components/Header.jsx
+++ React-Router/src/components/Header.jsx
@@ -2,11 +2,26 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
+
+        // -------------------------------------------------- Header Starts --------------------------------------------------------
+
         <header className="shadow sticky z-50 top-0">
+
+            {/* -------------------------------------- Navigation Links and Logo Starts --------------------------------------------------- */}
+
+
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
 
 
+                {/* -------------------------- Div for applying Flex Properties Start --------------------------------------------------------- */}
+
+
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+
+
+
+
+                    {/* ------------------------------------------ Logo Starts ---------------------------------------------------------------- */}
 
                     <Link to="/" className="flex items-center">
                         <img
@@ -16,31 +31,27 @@ export default function Header() {
                         />
                     </Link>
 
+                    {/* ------------------------------------------- Logo Ends ---------------------------------------------------------------- */}
 
-                    <div className="flex items-center lg:order-2">
 
-                        <Link
-                            to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log in
-                        </Link>
 
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
-                        </Link>
 
-                    </div>
+
+                    {/* ------------------------------------- Div for Home, About and Contact Links Start -------------------------------------*/}
+
 
                     <div
                         className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
                     >
+
+
+                        {/* ---------------------------------- Home, About and Contact Links Start -------------------------------------*/}
+
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
 
+
+                            {/* ------------------------------------------ Home Starts ------------------------------------------------- */}
 
                             <li>
                                 <NavLink to="/"
@@ -53,6 +64,14 @@ export default function Header() {
                             </li>
 
 
+                            {/* ------------------------------------------ Home Ends ------------------------------------------------- */}
+
+
+
+
+
+                            {/* ------------------------------------------ About Starts ------------------------------------------------- */}
+
                             <li>
                                 <NavLink to="/about"
                                     className={({ isActive }) =>
@@ -63,6 +82,11 @@ export default function Header() {
                                 </NavLink>
                             </li>
 
+                            {/* ------------------------------------------- About Ends ------------------------------------------------- */}
+
+
+
+                            {/* ------------------------------------------ Contact Starts ------------------------------------------------- */}
 
                             <li>
                                 <NavLink to="/contact"
@@ -74,14 +98,74 @@ export default function Header() {
                                 </NavLink>
                             </li>
 
+                            {/* ------------------------------------------ Contact Ends ------------------------------------------------- */}
+
+
 
                         </ul>
+
+                        {/* ---------------------------------- Home, About and Contact Links End -------------------------------------*/}
+
+
                     </div>
+
+
+                    {/* -------------------------------------- Div for Home, About and Contact Links End -------------------------------------*/}
+
+
+
+
+
+                    {/* --------------------------------------- Div for Log In and Get Started Start -------------------------------------*/}
+
+                    <div className="flex items-center lg:order-2">
+
+
+                        {/* ------------------------------------------ Log In Starts ---------------------------------------------------- */}
+
+                        <Link
+                            to="/"
+                            className="text-gray-800 hover:bg-gray-50 focus:ring-2 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Log In
+                        </Link>
+
+                        {/* ------------------------------------------ Log In Ends ---------------------------------------------------- */}
+
+
+
+
+                        {/* ------------------------------------------ Get Stared Starts ---------------------------------------------------- */}
+
+                        <Link
+                            to="/"
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Get Started
+                        </Link>
+
+                        {/* ------------------------------------------ Get Stared Ends ---------------------------------------------------- */}
+
+
+                    </div>
+
+                     {/* --------------------------------------- Div for Log In and Get Started End -------------------------------------*/}
+
 
                 </div>
 
+
+                {/* --------------------------- Div for applying Flex Properties End --------------------------------------------------------- */}
+
             </nav>
 
+
+            {/* --------------------------------------- Navigation Links and Logo Ends --------------------------------------------------- */}
+
         </header>
+
+        // --------------------------------------------------- Header Ends ---------------------------------------------------------
+
+
     );
 }
