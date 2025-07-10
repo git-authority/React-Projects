08c297e6770fd242cdcabf1aada8b84ea3e3fd5b
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-10 23:41:53 +0530

    Increased Code Readability for Contact Page

--- React-Router/src/components/Contact.jsx
+++ React-Router/src/components/Contact.jsx
@@ -1,16 +1,33 @@
 export default function Contact() {
     return (
+
+
         <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
+
             <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
                 <div className="mt-8 overflow-hidden">
+
                     <div className="grid grid-cols-1 md:grid-cols-2">
+
+
+                        {/* ----------------------------------- Left Side Content Starts ---------------------------------------- */}
+
                         <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
+
+
                             <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
                                 Get in touch:
                             </h1>
+
+
                             <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
                                 Fill in the form to start a conversation
                             </p>
+
+
+
+                            {/* ------------------------------- Location Starts ---------------------------------- */}
 
                             <div className="flex items-center mt-8 text-gray-600">
                                 <svg
@@ -40,6 +57,14 @@ export default function Contact() {
                                 </div>
                             </div>
 
+                            {/* ------------------------------- Location Ends ---------------------------------- */}
+
+
+
+
+
+                            {/* ------------------------------- Phone No Starts ---------------------------------- */}
+
                             <div className="flex items-center mt-4 text-gray-600">
                                 <svg
                                     fill="none"
@@ -62,6 +87,14 @@ export default function Contact() {
                                 </div>
                             </div>
 
+                            {/* ------------------------------- Phone No Ends ---------------------------------- */}
+
+
+
+
+
+                            {/* ------------------------------- Mail Starts ---------------------------------------------- */}
+
                             <div className="flex items-center mt-2 text-gray-600">
                                 <svg
                                     fill="none"
@@ -83,7 +116,20 @@ export default function Contact() {
                                     info@acme.org
                                 </div>
                             </div>
+
+                            {/* -------------------------------- Mail Ends ------------------------------------ */}
+
+
                         </div>
+
+                        {/* ----------------------------------- Left Side Content Ends ---------------------------------------- */}
+
+
+
+
+
+
+                        {/* ----------------------------------- Right Side Content Starts ---------------------------------------- */}
 
                         <form className="p-6 flex flex-col justify-center">
                             <div className="flex flex-col">
@@ -132,9 +178,19 @@ export default function Contact() {
                                 Submit
                             </button>
                         </form>
+
+                        {/* ----------------------------------- Right Side Content Ends ---------------------------------------- */}
+
+
                     </div>
+
                 </div>
+
             </div>
+
         </div>
+
+
+
     );
 }
