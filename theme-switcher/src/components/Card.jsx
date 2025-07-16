c6eb448482ab57fa656609bd669b07a3437d0a5f
Abhrasnata Ray <abhrasnataray@gmail.com>
2025-07-16 21:57:00 +0530

    Updated Card Component

--- theme-switcher/src/components/Card.jsx
+++ theme-switcher/src/components/Card.jsx
@@ -1,16 +1,36 @@
 export default function Card() {
     return (
+
+
+        // --------------------------------- Main Div ---------------------------------------
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
-            </a>
+
+            <div className="flex justify-center items-center">
+                {/* ------------------- Upper Part of Main Div - Course Image ----------------------- */}
+                <a href="/">
+                    <img className="p-8 rounded-t-lg h-[310px] w-[340px] object-cover" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fmax%2F2400%2F1*IC0JXUE3UEAfDfQnFyGtGA.jpeg&f=1&nofb=1&ipt=c03eb83561c7c8e7d889903bfb5660af1d3a60074d179d8e5de1df3df613ae29" alt="product_image1" />
+                </a>
+                {/* ----------------- Upper Part of Main Div - Course Image Ends --------------------- */}
+            </div>
+
+
+
+            {/* -------------------------- Lower Part of Main Div --------------------------- */}
             <div className="px-5 pb-5">
+
+
+                {/* --------------------------- Course Title ----------------------------- */}
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Complete DSA Course: Beginner to Advanced
                     </h5>
                 </a>
+
+
+
+                {/* -------------------------------- Ratings ------------------------- */}
                 <div className="flex items-center mt-2.5 mb-5">
+
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
                         aria-hidden="true"
@@ -20,6 +40,8 @@ export default function Card() {
                     >
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
+
+
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
                         aria-hidden="true"
@@ -29,6 +51,8 @@ export default function Card() {
                     >
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
+
+
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
                         aria-hidden="true"
@@ -38,6 +62,8 @@ export default function Card() {
                     >
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
+
+
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
                         aria-hidden="true"
@@ -47,6 +73,8 @@ export default function Card() {
                     >
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
+
+
                     <svg
                         className="w-4 h-4 text-gray-200 dark:text-gray-600"
                         aria-hidden="true"
@@ -56,20 +84,42 @@ export default function Card() {
                     >
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                     </svg>
+
+
+                    {/* --------------------- Rating Number ---------------------------- */}
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
                         4.0
                     </span>
+
+
                 </div>
+
+
+                {/* -------------------- Div for Course Price and Add to Cart ------------------ */}
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+
+                    {/* ---------------------------- Price ------------------------------------ */}
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">₹5999</span>
+
+
+                    {/* ------------------------- Add to Cart -------------------------------- */}
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        Add to cart
+                        Add to Cart
                     </a>
+
                 </div>
+
+
             </div>
+            {/* ------------------------ Lower Part of Main Div Ends ------------------------- */}
+
+
         </div>
+        // --------------------------------- Main Div Ends -------------------------------------------
+
+
     );
 }
